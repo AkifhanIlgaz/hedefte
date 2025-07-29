fee7172f0c359a978c21d816fcc0960b68fdfa6c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2025-07-29 23:56:25 +0300

    Add configuration loading and MongoDB connection functionality

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/AkifhanIlgaz/hedefte/internal/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connection string: ", cfg.Mongo.ConnString)
+
 	server := gin.Default()
 
 	server.GET("/health", func(ctx *gin.Context) {
@@ -16,7 +25,7 @@ func main() {
 		})
 	})
 
-	err := server.Run()
+	err = server.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type MongoConfig struct {
+	ConnString string `mapstructure:"connection_string"`
+}
+
+type AppConfig struct {
+	Mongo MongoConfig `mapstructure:"mongo"`
+}
+
+func Load() (AppConfig, error) {
+	var config AppConfig
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("internal/config/")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return config, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, fmt.Errorf("failed to unmarshal config file: %w", err)
+	}
+
+	return config, nil
+}
